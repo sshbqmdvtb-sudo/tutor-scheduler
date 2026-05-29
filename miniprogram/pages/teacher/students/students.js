c48feabbf70da6b073b464a3b3b35c102db3cd5b
admin <admin@tutor-system.com>
2026-05-29 11:17:38 +0800

    fix: add student modal, show phone, remove student id display

--- miniprogram/pages/teacher/students/students.js
+++ miniprogram/pages/teacher/students/students.js
@@ -1,8 +1,8 @@
 const api = require('../../../utils/api')
-const { showError } = require('../../../utils/util')
+const { showToast, showError, PEOPLE_TYPES } = require('../../../utils/util')
 
 Page({
-  data: { students: [], loading: true },
+  data: { students: [], loading: true, showModal: false, form: { name: '', grade: '', subject: '', phone: '', default_fee: 100, default_people_type: '一对一' }, PEOPLE_TYPES },
 
   onShow() { this.load() },
 
@@ -17,9 +17,33 @@ Page({
     }
   },
 
-  goAddStudent() {
-    wx.navigateTo({ url: '/pages/teacher/students/students' })
+  showAddModal() { this.setData({ showModal: true }) },
+
+  onFormInput(e) {
+    const { field } = e.currentTarget.dataset
+    this.setData({ ['form.' + field]: e.detail.value })
   },
+
+  onPeopleTypeChange(e) {
+    this.setData({ 'form.default_people_type': PEOPLE_TYPES[e.detail.value] })
+  },
+
+  async addStudent() {
+    const { name, phone } = this.data.form
+    if (!name) return showToast('请输入学生姓名')
+    this.setData({ loading: true })
+    try {
+      await api.addStudent(this.data.form)
+      showToast('添加成功')
+      this.setData({ showModal: false, form: { name: '', grade: '', subject: '', phone: '', default_fee: 100, default_people_type: '一对一' } })
+      this.load()
+    } catch (err) {
+      showError(err.message || '添加失败')
+      this.setData({ loading: false })
+    }
+  },
+
+  hideModal() { this.setData({ showModal: false }) },
 
   goDetail(e) {
     const id = e.currentTarget.dataset.id
